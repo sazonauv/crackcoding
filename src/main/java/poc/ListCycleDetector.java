af59aa16a8719515dfcd6b8889f9983738898a25
slava-sazonau <sazonauv@gmail.com>
2017-11-07 11:05:30 +0000

    u

---
+++ src/main/java/poc/ListCycleDetector.java
@@ -0,0 +1,29 @@
+package poc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by slava on 07/11/17.
+ */
+public class ListCycleDetector {
+
+    class Node {
+        int data;
+        Node next;
+    }
+
+
+    boolean hasCycle(Node head) {
+        List<Node> visits = new LinkedList<>();
+        Node current = head;
+        boolean hasCycle = false;
+        while (!hasCycle && current != null) {
+            visits.add(current);
+            current = current.next;
+            hasCycle = visits.contains(current);
+        }
+        return hasCycle;
+    }
+
+}
