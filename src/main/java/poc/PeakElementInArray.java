213275e9e4f4311824a8b4d58adab9c8ed93a796
slava-sazonau <sazonauv@gmail.com>
2017-11-28 18:04:14 +0000

    u

---
+++ src/main/java/poc/PeakElementInArray.java
@@ -0,0 +1,27 @@
+package poc;
+
+public class PeakElementInArray {
+
+    public int findPeakElementLinear(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if ((i == 0 || nums[i] > nums[i-1])
+                    && (i == nums.length - 1 || nums[i] > nums[i+1])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public int findPeakElement(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if ((i == 0 || nums[i] > nums[i-1])
+                    && (i == nums.length - 1 || nums[i] > nums[i+1])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+}
---
+++ src/main/java/poc/VowelsReverser.java
@@ -0,0 +1,50 @@
+package poc;
+
+import java.util.Scanner;
+
+public class VowelsReverser {
+
+    public static String reverseVowels(String s) {
+        char[] vowels = new char[] {
+                'a', 'e', 'i', 'o', 'u',
+                'A', 'E', 'I', 'O', 'U',
+        };
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right && left < chars.length && right >= 0) {
+            while (left < right && !isVowel(chars[left], vowels)) {
+                left++;
+            }
+            while (right > left && !isVowel(chars[right], vowels)) {
+                right--;
+            }
+            if (left == right) {
+                break;
+            }
+            char leftChar = chars[left];
+            chars[left] = chars[right];
+            chars[right] = leftChar;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
+
+    private static boolean isVowel(char ch, char[] vowels) {
+        for (int i=0; i<vowels.length; i++) {
+            if (ch == vowels[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        System.out.println(reverseVowels(s));
+    }
+
+}
