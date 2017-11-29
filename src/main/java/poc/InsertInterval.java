8fd13557a121f202fed100dd2dc5c8c530eb06ac
slava-sazonau <sazonauv@gmail.com>
2017-11-29 21:06:05 +0000

    u

---
+++ src/main/java/poc/InsertInterval.java
@@ -0,0 +1,148 @@
+package poc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by slava on 29/11/17.
+ */
+public class InsertInterval {
+
+    class Interval {
+      int start;
+      int end;
+      Interval() { start = 0; end = 0; }
+      Interval(int s, int e) { start = s; end = e; }
+    }
+
+
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<>();
+        Interval left = null;
+        Interval right = null;
+        for (Interval in : intervals) {
+            if (in.start <= newInterval.start) {
+                left = in;
+            }
+            if (in.end >= newInterval.end) {
+                right = in;
+                break;
+            }
+        }
+        if (left == null && right == null) {
+            res.add(newInterval);
+            return res;
+        }
+        if (left == null) {
+            if (right.start <= newInterval.end) {
+                Interval mergIn = new Interval(newInterval.start, right.end);
+                res.add(mergIn);
+                boolean add = false;
+                for (Interval in : intervals) {
+                    if (add) {
+                        res.add(in);
+                    }
+                    if (in.equals(right)) {
+                        add = true;
+                    }
+                }
+            } else {
+                res.add(newInterval);
+                boolean add = false;
+                for (Interval in : intervals) {
+                    if (in.equals(right)) {
+                        add = true;
+                    }
+                    if (add) {
+                        res.add(in);
+                    }
+                }
+            }
+        } else
+        if (right == null) {
+            if (left.end >= newInterval.start) {
+                boolean add = true;
+                for (Interval in : intervals) {
+                    if (in.equals(left)) {
+                        add = false;
+                    }
+                    if (add) {
+                        res.add(in);
+                    } else {
+                        break;
+                    }
+                }
+                Interval mergIn = new Interval(left.start, newInterval.end);
+                res.add(mergIn);
+            } else {
+                boolean add = true;
+                for (Interval in : intervals) {
+                    if (add) {
+                        res.add(in);
+                    } else {
+                        break;
+                    }
+                    if (in.equals(left)) {
+                        add = false;
+                    }
+                }
+                res.add(newInterval);
+            }
+        } else {
+            Interval mergIn = new Interval(newInterval.start, newInterval.end);
+            if (left.end >= newInterval.start) {
+                boolean add = true;
+                for (Interval in : intervals) {
+                    if (in.equals(left)) {
+                        add = false;
+                    }
+                    if (add) {
+                        res.add(in);
+                    } else {
+                        break;
+                    }
+                }
+                mergIn.start = left.start;
+            } else {
+                boolean add = true;
+                for (Interval in : intervals) {
+                    if (add) {
+                        res.add(in);
+                    } else {
+                        break;
+                    }
+                    if (in.equals(left)) {
+                        add = false;
+                    }
+                }
+            }
+
+            res.add(mergIn);
+
+            if (right.start <= newInterval.end) {
+                mergIn.end = right.end;
+                boolean add = false;
+                for (Interval in : intervals) {
+                    if (add) {
+                        res.add(in);
+                    }
+                    if (in.equals(right)) {
+                        add = true;
+                    }
+                }
+            } else {
+                boolean add = false;
+                for (Interval in : intervals) {
+                    if (in.equals(right)) {
+                        add = true;
+                    }
+                    if (add) {
+                        res.add(in);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
--- src/main/java/poc/MedianSlidingWindow.java
+++ src/main/java/poc/MedianSlidingWindow.java
@@ -5,11 +5,11 @@ import java.util.Comparator;
 
 public class MedianSlidingWindow {
 
-    class ArrayIndexComparator implements Comparator<Integer> {
+    static class ArrayIndexComparator implements Comparator<Integer> {
 
-        int[] array;
+        double[] array;
 
-        ArrayIndexComparator(int[] array) {
+        ArrayIndexComparator(double[] array) {
             this.array = array;
         }
 
@@ -27,18 +27,27 @@ public class MedianSlidingWindow {
 
 
 
-    public double[] medianSlidingWindow(int[] nums, int k) {
-        Integer[] sortedIndex = getIndexArray(nums.length);
-        ArrayIndexComparator comp = new ArrayIndexComparator(nums);
+    public static double[] medianSlidingWindow(int[] nums, int k) {
+        double[] array = getDoubleArray(nums);
+        Integer[] sortedIndex = getIndexArray(array.length);
+        ArrayIndexComparator comp = new ArrayIndexComparator(array);
         Arrays.sort(sortedIndex, comp);
-        double[] medians = new double[nums.length-k+1];
+        double[] medians = new double[array.length-k+1];
         for (int i=0; i<medians.length; i++) {
-            medians[i] = median(nums, sortedIndex, i, k);
+            medians[i] = median(array, sortedIndex, i, k);
         }
         return medians;
     }
 
-    private Integer[] getIndexArray(int length) {
+    private static double[] getDoubleArray(int[] nums) {
+        double[] array = new double[nums.length];
+        for (int i=0; i<nums.length;i++) {
+            array[i] = nums[i];
+        }
+        return array;
+    }
+
+    private static Integer[] getIndexArray(int length) {
         Integer[] indexArray = new Integer[length];
         for (int i=0; i<length;i++) {
             indexArray[i] = i;
@@ -46,15 +55,38 @@ public class MedianSlidingWindow {
         return indexArray;
     }
 
-    private double median(int[] nums, Integer[] sortedIndex, int start, int k) {
+    private static double median(double[] nums, Integer[] sortedIndex, int start, int k) {
         if (k <= 1) {
             return nums[start];
         }
-        if (k % 2 == 0) {
-            return (nums[sortedIndex[start + k/2]] + nums[sortedIndex[start + k/2 - 1]]) / 2;
-        } else {
-            return nums[sortedIndex[start + k/2]];
+        int count = 0;
+        int cur = 0;
+        int prev = 0;
+        for (int i=0; i<sortedIndex.length; i++) {
+            if (sortedIndex[i] >= start && sortedIndex[i] < start+k) {
+                count++;
+                prev = cur;
+                cur = i;
+            }
+            if (count > k/2) {
+                break;
+            }
         }
+        if (k % 2 == 0) {
+            return (nums[sortedIndex[cur]] + nums[sortedIndex[prev]]) / 2;
+        } else {
+            return nums[sortedIndex[cur]];
+        }
+    }
+
+    public static void main(String[] args) {
+        int k = 2;
+        int[] array = new int[]{2147483647, 2147483647};
+        Out.p(Arrays.toString(array));
+//        int[] array = new int[]{1, 2, 3, 4};
+//        int[] array = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        // correct: [1,-1,-1,3,5,6]
+        Out.p(Arrays.toString(medianSlidingWindow(array, k)));
     }
 
 }
