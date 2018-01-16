303f84777c34803335502e687bac0b9cf617f0fd
slava-sazonau <sazonauv@gmail.com>
2018-01-16 18:42:32 +0300

    u

---
+++ src/main/java/poc/DeleteAndEarn.java
@@ -0,0 +1,86 @@
+package poc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by slava on 19/12/17.
+ */
+public class DeleteAndEarn {
+
+    // in: 2 8 2 9 8 9 8 8 5 5 10 2 6 6 6 9 3 10 9 3
+    // out: 76
+
+    public int deleteAndEarn(int[] nums) {
+        if (nums.length <= 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        // sorted histogram
+        int valNumber = 1;
+        int cur = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] != cur) {
+                valNumber++;
+                cur = nums[i];
+            }
+        }
+        int[] vals = new int[valNumber];
+        int[] counts = new int[valNumber];
+        vals[0] = nums[0];
+        counts[0] = 1;
+        cur = nums[0];
+        int j = 0;
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] != cur) {
+                j++;
+                cur = nums[i];
+                vals[j] = nums[i];
+                counts[j] = 1;
+            } else {
+                counts[j]++;
+            }
+        }
+        // count earned points
+        int points = 0;
+        int i = vals.length-1;
+        while (i >= 0) {
+            if (i == 0 || vals[i] - vals[i-1] > 1) {
+                points += vals[i] * counts[i];
+                i--;
+            } else {
+                int maxPoints = vals[i] * counts[i];
+                if (i == 1 || vals[i-1] - vals[i-2] > 1) {
+                    if (maxPoints < vals[i-1] * counts[i-1]) {
+                        maxPoints = vals[i-1] * counts[i-1];
+                    }
+                    i-=2;
+                } else {
+                    if (maxPoints + vals[i-2] * counts[i-2] < vals[i-1] * counts[i-1]) {
+                        maxPoints = vals[i-1] * counts[i-1];
+                        i-=3;
+                    } else {
+                        i-=2;
+                    }
+                }
+                points += maxPoints;
+            }
+        }
+        return points;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        System.out.println(new DeleteAndEarn().deleteAndEarn(a));
+    }
+
+}
---
+++ src/main/java/poc/MatrixKthSmallest.java
@@ -0,0 +1,67 @@
+package poc;
+
+/**
+ * Created by slava on 19/12/17.
+ */
+public class MatrixKthSmallest {
+
+    public int kthSmallest(int[][] m, int k) {
+        if (k == 1) {
+            return m[0][0];
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<m.length; i++) {
+            for (int j=0; j<m[0].length; j++) {
+                if (max < m[i][j]) {
+                    max = m[i][j];
+                }
+                if (min > m[i][j]) {
+                    min = m[i][j];
+                }
+            }
+        }
+        int mid = (max + min) / 2;
+        int low = min;
+        int high = max;
+        while (high > low) {
+            int lcount = 0;
+            for (int i=0; i<m.length; i++) {
+                for (int j=0; j<m[0].length; j++) {
+                    if (m[i][j] <= mid) {
+                        lcount++;
+                    }
+                }
+            }
+            if (lcount > k) {
+                high = mid;
+            } else if (lcount < k) {
+                low = mid + 1;
+            } else {
+                break;
+            }
+            mid = (low + high) / 2;
+        }
+        int res = m[0][0];
+        for (int i=0; i<m.length; i++) {
+            for (int j=0; j<m[0].length; j++) {
+                if (res < m[i][j] && m[i][j] <= mid) {
+                    res = m[i][j];
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] m = new int[][] {
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+        };
+        int k = 8;
+        System.out.println(new MatrixKthSmallest().kthSmallest(m, k));
+    }
+
+}
